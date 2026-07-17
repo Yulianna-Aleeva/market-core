486c20fe58fb41d5211ace1fb0da5945b2c2d7b1
Yulianna-Aleeva <89673838388@mail.ru>
2026-07-18 00:16:11 +0500

    lesson 14.2: classes

--- src/category.py
+++ src/category.py
@@ -1,16 +1,10 @@
-from src.product import Product
-
-
 class Category:
-    name: str  # название
-    description: str  # описание
-    products: list  # список товаров категории
     category_count = 0  # количество категорий
     product_count = 0  # количество товаров
 
-    def __init__(self, name: str, description: str, products: list[Product] | None = None) -> None:
-        self.name = name
-        self.description = description
-        self.products = products if products else []
+    def __init__(self, name: str, description: str, products: list = None) -> None:
+        self.name = name  # название категории
+        self.description = description  # описание категории
+        self.products = products if products else []  # список товаров категории
         Category.category_count += 1
-        Category.product_count += sum(product.quantity for product in self.products)
+        Category.product_count += len(self.products)
