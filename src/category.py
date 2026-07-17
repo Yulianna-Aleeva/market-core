98e8112001bdcc9e3d3668899f3273ea889e3960
Yulianna-Aleeva <89673838388@mail.ru>
2026-07-17 15:20:29 +0500

    lesson 14.2: annotations & toml

--- src/category.py
+++ src/category.py
@@ -1,3 +1,6 @@
+from src.product import Product
+
+
 class Category:
     name: str  # название
     description: str  # описание
@@ -5,7 +8,7 @@ class Category:
     category_count = 0  # количество категорий
     product_count = 0  # количество товаров
 
-    def __init__(self, name, description, products=None):
+    def __init__(self, name: str, description: str, products: list[Product] | None = None) -> None:
         self.name = name
         self.description = description
         self.products = products if products else []
