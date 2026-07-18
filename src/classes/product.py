e0509a24b8a8cbc3616251eaee16c7defe1d2898
Yulianna-Aleeva <89673838388@mail.ru>
2026-07-18 08:01:38 +0500

    lesson 14.2: private

--- src/classes/product.py
+++ src/classes/product.py
@@ -2,5 +2,16 @@ class Product:
     def __init__(self, name: str, description: str, price: float, quantity: int) -> None:
         self.name = name  # название товара
         self.description = description  # описание товара
-        self.price = price  # цена товара
+        self.__price = price  # приватная цена товара
         self.quantity = quantity  # количество товаров в наличии
+
+    @property
+    def price(self) -> float:  # геттер цены
+        return self.__price
+
+    @price.setter
+    def price(self, value: float) -> None:  # сеттер цены
+        if value <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+            return
+        self.__price = value
